077adc64c19a5f09ac8fd5c24b0b0be91dcc72dd
jorge <jorgecalheiros.s@gmail.com>
2022-04-13 21:35:17 -0300

    readme

--- src/backend/conexao.ts
+++ src/backend/conexao.ts
@@ -1,9 +1,10 @@
 import mysql from "mysql2"
+import "dotenv/config"
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "usuarios"
+    host: process.env.HOST,
+    user: process.env.ROOT,
+    database: process.env.DATABASE
 })
 
 
